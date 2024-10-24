25472b5e1311a17172eaf95177a8bc94da90ef69
mabackma <marc.backman@edu.lapinamk.fi>
2024-10-24 03:14:10 +0300

    Trees for_each method created. Method uses Js callback function for each tree.

--- src/forest_property/trees.rs
+++ src/forest_property/trees.rs
@@ -1,6 +1,9 @@
-use wasm_bindgen::prelude::wasm_bindgen;
-
 use crate::forest_property::tree::Tree;
+
+use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::JsValue;
+use web_sys::js_sys::{self, Array};
+use js_sys::Function;
 
 #[wasm_bindgen]
 pub struct Trees {
@@ -10,7 +13,7 @@ pub struct Trees {
     species: Vec<u8>,
     height: Vec<f32>,
     status: Vec<u8>,
-    stand_number: Vec<u8>,
+    stand_number: Vec<u16>,
 }
 
 #[wasm_bindgen]
@@ -23,7 +26,7 @@ impl Trees {
         species: Vec<u8>,
         height: Vec<f32>,
         status: Vec<u8>,
-        stand_number: Vec<u8>,
+        stand_number: Vec<u16>,
     ) -> Trees {
         Trees {
             x,
@@ -43,42 +46,87 @@ impl Trees {
         self.species.push(tree.species());
         self.height.push(tree.tree_height());
         self.status.push(tree.tree_status() as u8);
-        self.stand_number.push(tree.stand_number() as u8);
+        self.stand_number.push(tree.stand_number() as u16);
     }
 
-    // Getters for each field
-    #[wasm_bindgen(getter)]
-    pub fn x(&self) -> Vec<f32> {
-        self.x.clone()
+    pub fn for_each(&self, callback: &Function) {
+        for i in 0..self.x.len() {
+            // Create a JS Array for the arguments
+            let tree_args = Array::new();
+            tree_args.push(&JsValue::from_f64(self.x[i] as f64));
+            tree_args.push(&JsValue::from_f64(self.y[i] as f64));
+            tree_args.push(&JsValue::from_f64(self.z[i] as f64));
+            tree_args.push(&JsValue::from(self.species[i]));
+            tree_args.push(&JsValue::from_f64(self.height[i] as f64));
+            tree_args.push(&JsValue::from(self.status[i]));
+            tree_args.push(&JsValue::from(self.stand_number[i]));
+
+            let this = JsValue::null();
+            callback.call1(&this, &tree_args).unwrap();
+        }
     }
 
-    #[wasm_bindgen(getter)]
-    pub fn y(&self) -> Vec<f32> {
-        self.y.clone()
+    // Getters for each field returning JavaScript arrays
+    #[wasm_bindgen]
+    pub fn x(&self) -> Array {
+        let array = Array::new();
+        for &val in &self.x {
+            array.push(&JsValue::from_f64(val as f64));
+        }
+        array
     }
 
-    #[wasm_bindgen(getter)]
-    pub fn z(&self) -> Vec<f32> {
-        self.z.clone()
+    #[wasm_bindgen]
+    pub fn y(&self) -> Array {
+        let array = Array::new();
+        for &val in &self.y {
+            array.push(&JsValue::from_f64(val as f64));
+        }
+        array
     }
 
-    #[wasm_bindgen(getter)]
-    pub fn specie(&self) -> Vec<u8> {
-        self.species.clone()
+    #[wasm_bindgen]
+    pub fn z(&self) -> Array {
+        let array = Array::new();
+        for &val in &self.z {
+            array.push(&JsValue::from_f64(val as f64));
+        }
+        array
     }
 
-    #[wasm_bindgen(getter)]
-    pub fn height(&self) -> Vec<f32> {
-        self.height.clone()
+    #[wasm_bindgen]
+    pub fn species(&self) -> Array {
+        let array = Array::new();
+        for &val in &self.species {
+            array.push(&JsValue::from_f64(val as f64));
+        }
+        array
     }
 
-    #[wasm_bindgen(getter)]
-    pub fn status(&self) -> Vec<u8> {
-        self.status.clone()
+    #[wasm_bindgen]
+    pub fn height(&self) -> Array {
+        let array = Array::new();
+        for &val in &self.height {
+            array.push(&JsValue::from_f64(val as f64));
+        }
+        array
     }
 
-    #[wasm_bindgen(getter)]
-    pub fn stand_number(&self) -> Vec<u8> {
-        self.stand_number.clone()
+    #[wasm_bindgen]
+    pub fn status(&self) -> Array {
+        let array = Array::new();
+        for &val in &self.status{
+            array.push(&JsValue::from_f64(val as f64));
+        }
+        array
+    }
+
+    #[wasm_bindgen]
+    pub fn stand_number(&self) -> Array {
+        let array = Array::new();
+        for &val in &self.stand_number {
+            array.push(&JsValue::from_f64(val as f64));
+        }
+        array
     }
 }
