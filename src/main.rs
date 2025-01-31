60878083556144ff602adf4c85165efd26c87251
mabackma <marc.backman@edu.lapinamk.fi>
2025-01-31 05:58:35 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,10 @@ pub fn forest_property_data_from_polygon(polygon_string: &str) -> ForestProperty
 }
 
 fn main() -> Result<(), Box<dyn Error>>{
-    let property = ForestPropertyData::from_xml_file("forestpropertydata.xml");
+    //let property = ForestPropertyData::from_xml_file("forestpropertydata.xml");
 
-    //let polygon_string = "393960.156%206801453.126,%20394798.608%206801657.878,%20394930.512%206801670.111,%20395028.723%206802116.858,%20394258.945%206801929.148,%20394261.711%206801810.541,%20394091.166%206801665.961,%20393960.156%206801453.126";
-    //let property = forest_property_data_from_polygon(polygon_string);
+    let polygon_string = "393960.156%206801453.126,%20394798.608%206801657.878,%20394930.512%206801670.111,%20395028.723%206802116.858,%20394258.945%206801929.148,%20394261.711%206801810.541,%20394091.166%206801665.961,%20393960.156%206801453.126";
+    let property = forest_property_data_from_polygon(polygon_string);
 
     let mut bbox = get_bounding_box_of_map(&property);
 
